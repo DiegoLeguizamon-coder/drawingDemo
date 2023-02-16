2ea19e599cc8237da032fe7207d9fbd842517d76
Diego <alejandrohuertas1992@gmail.com>
2023-02-15 21:52:37 -0800

    Initial Commit

---
+++ demodraw/demodrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  demodrawApp.swift
+//  demodraw
+//
+//  Created by MacBook Pro on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct demodrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
